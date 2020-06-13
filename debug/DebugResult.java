85164e680a6735c577b4dcea8cff5056cd5c1213
B Murray III <bmurr3@users.noreply.github.com>
2020-06-13 18:54:08 -0400

    Initial Commit of debug/DebugResult.java
    A wrapper class to hold the result of a unit test.

---
+++ debug/DebugResult.java
@@ -0,0 +1,62 @@
+/*
+ * A wrapper class to control the results of debugging within the unit test
+ * suite.
+ */
+package com.init.debug;
+
+/**
+ * DebugResult
+ * 
+ * @version 1.0 13 June 2020
+ * @author William Murray III
+ */
+class DebugResult {
+	/**
+	 * The message to be returned upon the failure of a unit test.
+	 */
+	private String resultMessage;
+	/**
+	 * The overall result of the debug tests.
+	 */
+	private boolean hasPassed;
+	
+	public DebugResult() {
+		this.resultMessage = "[ OK ]";
+		this.hasPassed = true;
+	}
+	
+	public void addResult(boolean arg0, String messageOnFailure)
+			throws UnitFailureException {
+		this.hasPassed = this.hasPassed && arg0;
+		
+		if (this.hasPassed == false) {
+			this.resultMessage = "[FAIL]: " + messageOnFailure;
+			throw new UnitFailureException();
+		}
+	}
+	
+	public String getResultMessage() {
+		return this.resultMessage;
+	}
+	
+	public boolean getResult() {
+		return this.hasPassed;
+	}
+}
+
+/**
+ * UnitFailureException
+ * 
+ * @version 1.0 13 June 2020
+ * @author William Murray III
+ */
+class UnitFailureException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6913369704042179014L;
+
+	public UnitFailureException() {
+		super("");
+	}
+}
