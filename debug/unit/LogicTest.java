60ed54a91b7e50ab3623848bf3a6f044b6770a37
B Murray III <bmurr3@users.noreply.github.com>
2020-06-13 21:23:58 -0400

    Correcting Unit Test for com.util.logic.Logic Class
    Initially failed to start the unit test for the Logic class. Correction
    required the removal of the Unit Test interface, to make all methods in
    the unit test static, and to initialize the result in the unit test.

--- debug/unit/LogicTest.java
+++ debug/unit/LogicTest.java
@@ -2,7 +2,6 @@ package com.init.debug.unit;
 
 import com.init.debug.DebugResult;
 import com.init.debug.UnitFailureException;
-import com.init.debug.UnitTest;
 import com.util.logic.Logic;
 
 /**
@@ -11,161 +10,144 @@ import com.util.logic.Logic;
  * @version 1.0 13 June 2020
  * @author William Murray III
  */
-public class LogicTest implements UnitTest {
-	private DebugResult result;
+public class LogicTest {
+	private static DebugResult result;
 
-	@Override
-	public DebugResult startTest() {
+	public static DebugResult startTest() {
+		LogicTest.result = new DebugResult();
+		
 		try {
-			this.testLogic();
-			this.testAnd();
-			this.testOr();
-			this.testXor();
+			LogicTest.testAnd();
+			LogicTest.testOr();
+			LogicTest.testXor();
 		} catch (UnitFailureException ufException) {
-			//Exception not required.
+			return LogicTest.result;
 		}
 		
-		return this.result;
-	}
-
-	@Override
-	public void createTestObjects() {
-		// No Test Objects Required
-	}
-
-	@Override
-	public void destroyTestObjects() {
-		// No Test Objects Required
+		return LogicTest.result;
 	}
 	
-	private void testLogic() throws UnitFailureException {
-		String testLabel;
-		
-		testLabel = "Logic.class == Logic.class";
-		this.result.addResult(Logic.class == Logic.class, testLabel);
-	}
-	
-	private void testAnd() throws UnitFailureException {
+	private static void testAnd() throws UnitFailureException {
 		String testLabel;
 		
 		testLabel = "and(false) == false";
-		this.result.addResult(Logic.and(false) == false, testLabel);
+		LogicTest.result.addResult((Logic.and(false) == false), testLabel);
 		
 		testLabel = "and(true) == true";
-		this.result.addResult(Logic.and(true), testLabel);
+		LogicTest.result.addResult(Logic.and(true), testLabel);
 		
 		testLabel = "and(false, false) == false";
-		this.result.addResult(Logic.and(false, false) == false, testLabel);
+		LogicTest.result.addResult(Logic.and(false, false) == false, testLabel);
 		
 		testLabel = "and(false, true) == false";
-		this.result.addResult(Logic.and(false, true) == false, testLabel);
+		LogicTest.result.addResult(Logic.and(false, true) == false, testLabel);
 		
 		testLabel = "and(true, false) == false";
-		this.result.addResult(Logic.and(true, false) == false, testLabel);
+		LogicTest.result.addResult(Logic.and(true, false) == false, testLabel);
 		
 		testLabel = "and(true, true) == true";
-		this.result.addResult(Logic.and(true, true) == true, testLabel);
+		LogicTest.result.addResult(Logic.and(true, true) == true, testLabel);
 		
 		testLabel = "and(false, false, false) == false";
-		this.result.addResult(Logic.and(false, false, false) == false,
+		LogicTest.result.addResult(Logic.and(false, false, false) == false,
 				testLabel);
 		
 		testLabel = "and(false, false, true) == false";
-		this.result.addResult(Logic.and(false, false, true) == false,
+		LogicTest.result.addResult(Logic.and(false, false, true) == false,
 				testLabel);
 		
 		testLabel = "and(false, true, false) == false";
-		this.result.addResult(Logic.and(false, true, false) == false,
+		LogicTest.result.addResult(Logic.and(false, true, false) == false,
 				testLabel);
 		
 		testLabel = "and(false, true, true) == false";
-		this.result.addResult(Logic.and(false, true, true) == false,
+		LogicTest.result.addResult(Logic.and(false, true, true) == false,
 				testLabel);
 		
 		testLabel = "and(true, false, false) == false";
-		this.result.addResult(Logic.and(true, false, false) == false,
+		LogicTest.result.addResult(Logic.and(true, false, false) == false,
 				testLabel);
 		
 		testLabel = "and(true, false, true) == false";
-		this.result.addResult(Logic.and(true, false, true) == false,
+		LogicTest.result.addResult(Logic.and(true, false, true) == false,
 				testLabel);
 		
 		testLabel = "and(true, true, false) == false";
-		this.result.addResult(Logic.and(true, true, false) == false,
+		LogicTest.result.addResult(Logic.and(true, true, false) == false,
 				testLabel);
 		
 		testLabel = "and(true, true, true) == true";
-		this.result.addResult(Logic.and(true, true, true) == true,
+		LogicTest.result.addResult(Logic.and(true, true, true) == true,
 				testLabel);
 	}
 	
-	private void testOr() throws UnitFailureException {
+	private static void testOr() throws UnitFailureException {
 		String testLabel;
 		
 		testLabel = "or(false) == false";
-		this.result.addResult(Logic.or(false) == false, testLabel);
+		LogicTest.result.addResult(Logic.or(false) == false, testLabel);
 		
 		testLabel = "or(true) == true";
-		this.result.addResult(Logic.or(true), testLabel);
+		LogicTest.result.addResult(Logic.or(true), testLabel);
 		
 		testLabel = "or(false, false) == false";
-		this.result.addResult(Logic.or(false, false) == false, testLabel);
+		LogicTest.result.addResult(Logic.or(false, false) == false, testLabel);
 		
 		testLabel = "or(false, true) == true";
-		this.result.addResult(Logic.or(false, true) == true, testLabel);
+		LogicTest.result.addResult(Logic.or(false, true) == true, testLabel);
 		
 		testLabel = "or(true, false) == true";
-		this.result.addResult(Logic.or(true, false) == true, testLabel);
+		LogicTest.result.addResult(Logic.or(true, false) == true, testLabel);
 		
 		testLabel = "or(true, true) == true";
-		this.result.addResult(Logic.or(true, true) == true, testLabel);
+		LogicTest.result.addResult(Logic.or(true, true) == true, testLabel);
 		
 		testLabel = "or(false, false, false) == false";
-		this.result.addResult(Logic.or(false, false, false) == false,
+		LogicTest.result.addResult(Logic.or(false, false, false) == false,
 				testLabel);
 		
 		testLabel = "or(false, false, true) == true";
-		this.result.addResult(Logic.or(false, false, true) == true,
+		LogicTest.result.addResult(Logic.or(false, false, true) == true,
 				testLabel);
 		
 		testLabel = "or(false, true, false) == true";
-		this.result.addResult(Logic.or(false, true, false) == true,
+		LogicTest.result.addResult(Logic.or(false, true, false) == true,
 				testLabel);
 		
 		testLabel = "or(false, true, true) == true";
-		this.result.addResult(Logic.or(false, true, true) == true,
+		LogicTest.result.addResult(Logic.or(false, true, true) == true,
 				testLabel);
 		
 		testLabel = "or(true, false, false) == true";
-		this.result.addResult(Logic.or(true, false, false) == true,
+		LogicTest.result.addResult(Logic.or(true, false, false) == true,
 				testLabel);
 		
 		testLabel = "or(true, false, true) == true";
-		this.result.addResult(Logic.or(true, false, true) == true,
+		LogicTest.result.addResult(Logic.or(true, false, true) == true,
 				testLabel);
 		
 		testLabel = "or(true, true, false) == true";
-		this.result.addResult(Logic.or(true, true, false) == true,
+		LogicTest.result.addResult(Logic.or(true, true, false) == true,
 				testLabel);
 		
 		testLabel = "or(true, true, true) == true";
-		this.result.addResult(Logic.or(true, true, true) == true,
+		LogicTest.result.addResult(Logic.or(true, true, true) == true,
 				testLabel);
 	}
 	
-	private void testXor() throws UnitFailureException {
+	private static void testXor() throws UnitFailureException {
 		String testLabel;
 		
 		testLabel = "xor(false, false) == false";
-		this.result.addResult(Logic.and(false, false) == false, testLabel);
+		LogicTest.result.addResult(Logic.xor(false, false) == false, testLabel);
 		
 		testLabel = "xor(false, true) == true";
-		this.result.addResult(Logic.and(false, true) == true, testLabel);
+		LogicTest.result.addResult(Logic.xor(false, true) == true, testLabel);
 		
 		testLabel = "xor(true, false) == true";
-		this.result.addResult(Logic.and(true, false) == true, testLabel);
+		LogicTest.result.addResult(Logic.xor(true, false) == true, testLabel);
 		
 		testLabel = "xor(true, true) == false";
-		this.result.addResult(Logic.and(true, true) == false, testLabel);
+		LogicTest.result.addResult(Logic.xor(true, true) == false, testLabel);
 	}
 }
